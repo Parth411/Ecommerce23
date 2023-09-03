9e41d2ab6ab6298b1a9b88d92943d1521400eec1
Parth411 <parthvichhi4@gmail.com>
2023-09-03 14:34:38 +0530

    Till Login Completed...

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const {registerUser, loginUser} = require("../controllers/userController");
 
+
+//register
 router.route("/register").post(registerUser);
 
+//user
 router.route("/login").post(loginUser);
 
 module.exports = router;
